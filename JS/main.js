d190d15b9e3d266b41c833861064b7bced2103b2
jonibek0979 <omadbekxoshimov20060715@gmail.com>
2023-11-20 13:43:29 +0500

    inital commit

---
+++ JS/main.js
@@ -0,0 +1,73 @@
+const arr = ['ol4ma','beh2i','no3k','a1nor']
+
+arr.forEach((item)=>{
+   for (let i = 0; i < item.length; i++) {
+    if (item[i] * 2 >= 0) {
+      console.log(item[i]);
+    }
+   
+   }
+})
+
+
+
+if (arr == arr.toString()) {
+ let b = arr.sort();
+console.log(b.toString());
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
